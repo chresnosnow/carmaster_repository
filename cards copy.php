14c7eecec832189e480c8f216f0ddbcb7734f043
christian josue campos granados <chris.campos7483@gmail.com>
2021-05-27 20:52:02 -0600

    edicion de diseño

--- cards copy.php
+++ cards copy.php
@@ -377,8 +377,8 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                                Paso 1: </div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Verificar Clientes</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -395,8 +395,8 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Paso 2: </div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Registrar Clientes</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -412,11 +412,11 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Paso 3: 
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Abir un Contrato</div>
                                                 </div>
                                                 <div class="col">
                                                     <div class="progress progress-sm mr-2">
@@ -442,8 +442,8 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                                Paso 4: </div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Notificarle al Cliente</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-comments fa-2x text-gray-300"></i>
@@ -461,7 +461,7 @@
                             <!-- Default Card Example -->
                             <div class="card mb-4">
                                 <div class="card-header font-weight-bold text-primary" style="text-align: center;" >
-                                    Actualizar Usuario Verificado
+                                    Actualizar Cliente Verificado
                                 </div>
                                 <div class="card-body">
                                     <p>La información del número de DUI ingresado es la siguiente: </p>
