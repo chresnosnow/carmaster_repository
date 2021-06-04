130b5f71d09d8808794e095ba31a8c7cadeab31e
chresnosnow <chris.granados7483@gmail.com>
2021-06-04 17:44:27 -0600

    Devolucion de contrato a partir de estado

---
+++ cards devupdatea.php
@@ -0,0 +1,19 @@
+<?php
+include 'conexion.php';
+include 'contrato_form.php';
+if (!$link) {
+	die('No se ha podido conectar a la base de datos');
+}
+$insert_value = 'UPDATE `' . $db_name . '`.`'.$db_table_name.'` SET `id_contrato` = "' . $subs_contr . '" , `id_placa` = "' . $subs_placa . '" , `nombre_cliente` = "' . $subs_namlas . '" , `fechini` = "' . $subs_fechi . '" , `fechafin` = "' . $subs_fechf . '" , `costo` = "' . $subs_cost . '" , `estado` = "' . $subs_esta . '" WHERE `id_dui` = "' . $subs_dui . '" ';
+
+mysqli_select_db($link, $db_name);
+$retry_value = mysqli_query($link, $insert_value);
+
+if (!$retry_value) {
+   die('Error: ' . mysqli_error($link));
+}
+
+mysqli_close($link);
+header('Location: cards devo.html');
+		
+?>
